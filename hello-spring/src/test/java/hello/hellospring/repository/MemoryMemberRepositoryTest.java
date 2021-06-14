a8f6a0124a56685ecfd4db35979a52b2403c7180
장명수 <dev.maeng@gmail.com>
2021-06-14 16:42:44 +0900

    doc: 테스트 이름찾기 케이스 추가

--- hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -3,6 +3,7 @@ package hello.hellospring.repository;
 import hello.hellospring.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.stereotype.Repository;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -19,5 +20,17 @@ class MemoryMemberRepositoryTest {
 
         Member result = repository.findById(member.getId()).get();
         assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
     }
 }
