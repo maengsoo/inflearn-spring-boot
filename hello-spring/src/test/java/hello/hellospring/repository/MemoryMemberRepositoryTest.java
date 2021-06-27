1a8dd72e7006b04162476ebba244cf09b5b0c894
장명수 <dev.maeng@gmail.com>
2021-06-27 16:04:33 +0900

    feat: MemoryMemberRepositoryTest, MemberServiceTest 테스트케이스 구현 및 실행
    - D.I 의존성 주입 공부

--- hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -2,14 +2,22 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 class MemoryMemberRepositoryTest {
 
     MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
 
     @Test
     public void save(){
@@ -32,10 +40,23 @@ class MemoryMemberRepositoryTest {
         member2.setName("spring2");
         repository.save(member2);
 
-        Member member3 = new Member();
-        member3.setName("spring3");
-        repository.save(member3);
+        Member result = repository.findByName("spring1").get();
 
+        assertThat(result).isEqualTo(member1);
+    }
 
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring1");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
     }
 }
